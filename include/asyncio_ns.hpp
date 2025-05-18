79f23c921827c579617f43803569b1faa84fd61d
kewuaa <202321011111@std.uestc.edu.cn>
2025-05-18 09:22:25 +0800

    add ASYNCIO_NS macro

--- include/asyncio_ns.hpp
+++ include/asyncio_ns.hpp
@@ -1,3 +1,4 @@
 #pragma once
-#define ASYNCIO_NS_BEGIN(...) namespace kwa::asyncio __VA_OPT__(::) __VA_ARGS__ {
+#define ASYNCIO_NS kwa::asyncio
+#define ASYNCIO_NS_BEGIN(...) namespace ASYNCIO_NS __VA_OPT__(::) __VA_ARGS__ {
 #define ASYNCIO_NS_END }
